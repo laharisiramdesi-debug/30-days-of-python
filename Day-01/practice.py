5caffb32ac8219926bd56a9e96064ff5c7882225
laharisiramdesi-debug <laharisiramdesi@gmail.com>
2026-07-20 20:20:10 +0530

    Add files via upload

---
+++ Day-01/practice.py
@@ -0,0 +1,23 @@
+#calculating simple interest
+p=int(input("p:"))
+r=int(input("r:"))
+t=int(input("t:"))
+si=(p*r*t)/100
+print("simpleinterest:",si)
+#calculate area of circle
+r=int(input("r:"))
+pi=22/7
+area=pi * (r**2)
+print("Area:",area)
+#convert celsius to farenheit
+c=float(input("celsius:"))
+f=(9/5)*c+32
+print("farenheit:",f)
+#convert farenheit to celsius
+f=float(input("f:"))
+c=(5/9)*(f-32)
+print("celsius:",c)
+#find the square root of a number
+number=float(input("enter number:"))
+squareroot=number**0.5
+print(squareroot)
