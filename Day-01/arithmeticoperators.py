ac2a883594c1173870601a4b7c592c6a9ced36f3
laharisiramdesi-debug <laharisiramdesi@gmail.com>
2026-07-20 20:18:23 +0530

    Add files via upload

---
+++ Day-01/arithmeticoperators.py
@@ -0,0 +1,19 @@
+a=int(input("a:"))
+b=int(input("b:"))
+print("a+b:",a+b)
+print("a-b:",a-b)
+print("a*b:",a*b)
+print("a/b:",a/b)
+print("a%b:",a%b)
+print("a//b:",a//b)
+print("a**b:",a**b)
+#area of rectangle
+l=a
+w=b
+print("area of a rectangle:",l*w)
+#average of a number
+c=int(input("c:"))
+d=(a+b+c)/3
+print("average of three numbers:",d)
+#cube of a number
+print("cube of a number:",c**3)
